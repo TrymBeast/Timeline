f7088f0288ab958235eb2f3652f9ea33814ba578
André Matos <apdmatos@gmail.com>
2012-11-16 00:43:59 +0000

    added support for image and different event types such as images and maps

--- js/timelinepatch.js
+++ js/timelinepatch.js
@@ -5,5 +5,5 @@
 Timeline.CompactEventPainter.prototype._showBubble = function(x, y, evt) {
     
     // use the obj to show the light window
-    alert (evt[0]._obj.title);
+    alert (evt[0]._obj.title + ": " + evt[0]._obj.image);
 } 
