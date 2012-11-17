ff49751ee386056296fb9679defd134d285988a3
Tiago Ribeiro <trymbeast@gmail.com>
2012-11-17 02:58:04 +0000

    Added isolated webcam snapshot

--- js/webcam.js
+++ js/webcam.js
@@ -1,18 +1,58 @@
 
 var webcam = {
 
-	video: null,
-	button: null,
+	videoElement: null,
+	snapshotButton: null,
+	imgElement: null,
+	stream: null,
 	
-	init: function(video, button)
+	init: function(videoElement, snapshotButton, imgElement)
 	{
-		this.video = video;
-		this.button = button;
+		this.videoElement = $(videoElement);
+		this.snapshotButton = $(snapshotButton);
+		this.imgElement = $(imgElement);
+		
+		this.snapshotButton.click(webcam.snapshot);
 	},
 	show: function() {
-		$('#webcam').fadeTo("slow", 0.8, webcam.start);
+		$('#webcam').fadeTo("slow", 0.8, function(){webcam.start();});
+		$('#webcam').click(function(){webcam.hide();});
 	},
 	hide: function() {
-		$('#webcam').fadeTo("slow", 0, webcam.stop);
+		$('#webcam').fadeTo("slow", 0, function(){webcam.stop();});
+	},
+	start: function() {
+		var onFailSoHard = function(e) {
+			console.log('Reeeejected!', e);
+		};
+
+		navigator.getUserMedia  = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+								  navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+		navigator.getUserMedia({video: true, audio: false}, function(stream) {
+			
+			webcam.videoElement.attr('src',window.URL.createObjectURL(stream));
+			webcam.stream = stream;
+			
+		}, onFailSoHard);
+	},
+	stop: function() {
+		webcam.videoElement.attr('src','');
+	},
+	snapshot: function(evt) {
+		if(webcam.stream){
+			var video = webcam.videoElement[0];
+			var width = webcam.videoElement.width();
+			var height = video.videoHeight / (video.videoWidth/width);
+			var canvas = $('canvas')[0];
+			canvas.width = width;
+			canvas.height = height;
+			var ctx = canvas.getContext('2d');
+			ctx.drawImage(video, 0, 0, width, height);
+			// "image/webp" works in Chrome 18. In other browsers, this will fall back to image/png.
+			webcam.imgElement.attr('src', canvas.toDataURL('image/webp'));
+		}
+
+		return false;
 	}
 };
